9f4faeb94c5dc9f41fa9fbfb7ad9b98fd085e7eb
Dan Leehr <dan.leehr@duke.edu>
2015-05-19 11:05:25 -0400

    Fix handling of args

--- docker-pipeline.sh
+++ docker-pipeline.sh
@@ -4,4 +4,9 @@
 # First argument should be the absolute path to the YAML file.
 # All other arguments will be passed to pipeline.py
 
-docker run -v /var/run/docker.sock:/var/run/docker.sock -v "$1":/pipeline.yaml:ro dukegcb/docker-pipeline /pipeline.yaml "${*:2}"
+if [ -z "$1" ]; then
+  echo "Please provide the absolute path to a YAML file"
+  exit 1
+fi
+
+docker run -v /var/run/docker.sock:/var/run/docker.sock -v $1:/pipeline.yaml:ro dukegcb/docker-pipeline /pipeline.yaml ${*:2}
