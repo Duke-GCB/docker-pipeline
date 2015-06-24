0538beefb4f46fb96f80ae2aa7faafc5119b4e3d
Dan Leehr <dan.leehr@duke.edu>
2015-06-24 15:42:49 -0400

    Add setup.py for distutils

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='docker-pipeline',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/Duke-GCB/docker-pipeline',
+    license='MIT',
+    author='dcl9',
+    author_email='dan.leehr@duke.edu',
+    description='Run docker containers in sequence, for reproducible computational analysis pipelines '
+)
